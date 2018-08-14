ca81e2d79898772b8190d88463699c89cc5d2029
SUHAILAZ <suhailaka.28@gmail.com>
2018-08-14 16:16:02 +0530

    first commit

--- scripts/mark-the-letters.js
+++ scripts/mark-the-letters.js
@@ -9,22 +9,51 @@ function MarkTheLetters(params,id){
   this.params = params;
 
   var html = self.params.textField;
-
+  var str =self.params.textField;
+  str = $(str).text();
   self.attach = function($container){
     $container.append('<div class="task-description">'+ self.params.question +'</div>');
     var str1=[];
     var answer = self.params.solution;
     answer=answer.replace(/\,/g,"");
+    var result = [];
+    if($(answer).val()!==null)
+    {
+      for(i=0;i<str.length;i++){
+        for(var j=0;j<answer.length;j++)
+        {
+          if(str[i]==answer[j]){
+            result.push(str[i]);
+          }
 
-    var div = document.createElement("div");
-    div.innerHTML = html;
-    var str = div.innerText;
-    // $container.append('<div class="text-field">'+ str +'</div>');
+        }
+
+      }
+      console.log(result);
+    }
+
+
+    console.log(result.length);
+
+    str1=str;
+    var input=[];
+    var index= [];
+    input=str.match(/(\*.\*)/g);
+    for(i=0;i<input.length;i++)
+    {
+      input[i]=input[i].replace(/(\*)/g,"");
+      index.push(str.indexOf(input[i]));
+    }
+
+    str=str.replace(/(\*)/g,"");
+    // str = $(str).text();
+    console.log(index);
+    console.log(str);
+
     for (var i = 0; i < str.length; i++) {
-
-        var nod = str[i];
+      var node = str[i];
         console.log(str.charCodeAt(i));
-        var $li = $('<li>'+ nod +'</li>').appendTo($container);
+        var $li = $('<li>'+ node +'</li>').appendTo($container);
         if(str.charCodeAt(i)!==32)
         {
           if(str.charCodeAt(i)!==44)
@@ -33,41 +62,20 @@ function MarkTheLetters(params,id){
               $li.addClass("new-li");
             }
           }
-
         }
         else{
           $li.removeClass("newli");
         }
-
-
-
-
         $li.click(function(){
-                    $(this).addClass("div-alpha");
-                    // $(this).attr("disabled", true);
-                    var flag = 0;
-                    for (i = 0; i < answer.length; i++) {
-
-                        if (answer[i] == this.innerHTML) {
-
-                            console.log(this.innerHTML);
-                            flag = 1;
-
-                        }
-
-
-                      }
-
-          });
-
-        // console.log(nod);
+            console.log("wrking");
+            for(k=0;k<index.length;k++){
+              if(this.innerHTML==str[index[k]]){
+                console.log("correct");
+              }
+            }
+        });
     }
-
-
-
 console.log(answer);
-
-
   }
 }
 
