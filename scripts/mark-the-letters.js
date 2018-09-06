68c5e0959621f95e434fc5c06069c85b9d30da4e
SUHAILAZ <suhailaka.28@gmail.com>
2018-09-06 14:29:33 +0530

    second commit

--- scripts/mark-the-letters.js
+++ scripts/mark-the-letters.js
@@ -15,12 +15,39 @@ H5P.MarkTheLetters = (function($, Question, UI) {
         var clickedLetters = [];
         var correctAnswer = [];
         var wrongAnswer = [];
+        var score = 0;
+        var maxScore;
+        var defaults = {
+           image: null,
+           question: "No question text provided",
+           answers: [
+             {
+               tipsAndFeedback: {
+                 tip: '',
+                 chosenFeedback: '',
+                 notChosenFeedback: ''
+               },
+               text: "Answer 1",
+               correct: true
+             }
+           ],
+           overallFeedback: [],
+           weight: 1,
+           userAnswers: [],
+           UI: {
+             checkAnswerButton: 'Check',
+             showSolutionButton: 'Show solution',
+             tryAgainButton: 'Try again',
+             scoreBarLabel: 'You got :num out of :total points'
+           }
+        };
         // var ans = [];
-        var checkAnswer = function($container, $ul, $li, correctAnswer, wrongAnswer,clickedLetters,$checkButtonContainer) {
+        var checkAnswer = function($container,$ul, $li, correctAnswer, wrongAnswer,clickedLetters,$checkButtonContainer,index) {
             // console.log(correctAnswer);
             // console.log(wrongAnswer);
             // console.log(clickedLetters);
-            $checkButtonContainer.empty();
+            maxScore = index.length;
+            $checkButtonContainer.hide();
             var dataList = $(".list li").map(function() {
                 return $(this).data("id");
             }).get();
@@ -29,41 +56,89 @@ H5P.MarkTheLetters = (function($, Question, UI) {
                 for (j = 0; j < correctAnswer.length; j++) {
                     if (el == correctAnswer[j]) {
                         $(this).attr("aria-describedby", "h5p-description-correct");
+                        $(this).addClass("correct-position");
                         $(this).append('<div class="question-plus-one"></div>');
+                        score++;
+
+                        var qHeight= $(this).height();
+                        var pos=$(this).position();
+                        console.log(pos.top-qHeight );
                     }
                 }
-
                 for (j = 0; j < wrongAnswer.length; j++) {
                     if (el == wrongAnswer[j]) {
                         $(this).attr("aria-describedby", "h5p-description-incorrect");
+                        $(this).addClass("incorrect-position");
                         $(this).append('<div class="question-minus-one"></div>');
+                        var iTop=$(".incorrect-position").position();
                     }
                 }
             });
 
+            console.log(score);
+            console.log(maxScore);
+
+
+              self.$retry = UI.createButton({
+                title: 'Retry Button',
+                'text' : self.params.tryAgainButton,
+                'class': 'retry h5p-question-try-again h5p-joubelui-button',
+                click : function(){
+                  $container.empty();
+                  self.attach($container);
+                },
+
+              });
+              var $showSolutionButtonContainer = $('<div class="retry-button-container" />');
+              self.$showSolution = UI.createButton(
+                {
+                  title: 'Show Solution Button',
+                  'text' : self.params.showSolutionButton,
+                  'class': 'show-asolutionr h5p-question-show-solution h5p-joubelui-button',
+                  click : function(){
+                    $(".question-plus-one").hide();
+                    $(".question-minus-one").hide();
+                    $(this).hide();
+
+                     $("li").each(function(el){
+                       for(i=0;i<index.length;i++){
+                         if($(this).attr("data-id")==index[i]){
+                           $(this).addClass("div-alpha");
+                         }
+                       }
+                     });
+                  },
+                });
+
+            var $scoreLabel = $('<div></div>').appendTo($container);
+
+
             if(correctAnswer.length===0  && clickedLetters.length>0){
               console.log("incorrect");
-              // var $checkButtonContainer = $('<div class="button-container" />').appendTo($container);
-              // self.$checkAnswer = UI.createButton({
-              //     title: 'Button',
-              //     'class': 'check-answer h5p-question-check-answer h5p-joubelui-button',
-              //     'text': 'Check',
-              //     click: function() {
-              //         $(this).attr("disabled", true);
-              //         checkAnswer($container, $ul, $li, correctAnswer, wrongAnswer, clickedLetters);
-              //     },
-              // }).appendTo($checkButtonContainer);
+
+              self.$retry.appendTo($container);
+              self.$showSolution.appendTo($container);
+
             }
 
             if(wrongAnswer.length===0 && clickedLetters.length>0){
               console.log("correct");
+              if(correctAnswer.length!=index.length){
+                self.$retry.appendTo($container);
+                self.$showSolution.appendTo($container);
+              }
             }
 
             if( correctAnswer.length>0 && wrongAnswer.length >0){
               console.log("mixed");
+              if(correctAnswer.length==index.length){
+                self.$retry.appendTo($container);
+              }
+              else{
+                self.$retry.appendTo($container);
+                self.$showSolution.appendTo($container);
+              }
             }
-
-
         }
 
         self.attach = function($container) {
@@ -157,11 +232,10 @@ H5P.MarkTheLetters = (function($, Question, UI) {
                 'text': self.params.checkAnswerButton,
                 click: function() {
                     $(this).attr("disabled", true);
-                    checkAnswer($container, $ul, $li, correctAnswer, wrongAnswer, clickedLetters, $checkButtonContainer);
+                    checkAnswer($container, $ul, $li, correctAnswer, wrongAnswer, clickedLetters, $checkButtonContainer,index);
                 },
             }).appendTo($checkButtonContainer);
         }
     }
-
     return MarkTheLetters;
 })(H5P.jQuery, H5P.Question, H5P.JoubelUI);
